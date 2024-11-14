f8d883a62adea27bc172544e1a368cd9d0026aca
Bikash Kumar Dhal <bikashdhal006@gmail.com>
2024-11-14 22:52:29 +0530

    update for Tutorial

---
+++ DataStructure/ds/test/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+public class SinglyLinkedList {
+	
+	private String data;
+	private SinglyLinkedList head;
+	
+	SinglyLinkedList(String data,SinglyLinkedList head ){
+		this.data=data;
+		head=null;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
