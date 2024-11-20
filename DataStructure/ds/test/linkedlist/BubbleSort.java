7220810d325b3d31ca074aceb3a0f9ac7126b2df
Bikash Kumar Dhal <bikashdhal006@gmail.com>
2024-11-20 23:15:38 +0530

    BubbleSort

---
+++ DataStructure/ds/test/linkedlist/BubbleSort.java
@@ -0,0 +1,34 @@
+package linkedlist;
+
+public class BubbleSort {
+	
+	public int[] sort(int[] list) {
+		
+		for(int i=0;i<list.length;i++) {
+			//int j =i;
+			for(int j=i+1;j<list.length;j++) {
+				if(list[i]>list[j]) {
+					int temp = list[j];
+					list[j]=list[i];
+					list[i]=temp;
+				}
+			}
+			
+		}
+		return list;
+	}
+	
+	public void print(int[]list) {
+		for(int i=0;i<list.length;i++)
+			System.out.println(list[i]);
+	}
+
+	public static void main(String[] args) {
+		int[]a= {545,6,1};
+		BubbleSort ins = new BubbleSort();
+		a=ins.sort(a);
+		ins.print(a);
+
+	}
+
+}
