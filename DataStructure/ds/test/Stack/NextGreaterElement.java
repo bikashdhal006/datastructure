bd31a4ca95b7ce293505832824f7efc8c77f183a
Bikash Kumar Dhal <bikashdhal006@gmail.com>
2024-12-04 12:36:10 +0530

    update

--- DataStructure/ds/test/Stack/NextGreaterElement.java
+++ DataStructure/ds/test/Stack/NextGreaterElement.java
@@ -7,13 +7,27 @@ public class NextGreaterElement {
 	private void getNextGreaterLementOnRight(Integer[] arr) {
 		Stack<Integer> s =new Stack<>();
 		for(int i=0;i<arr.length;i++) {
+			int next = arr[i];
 			if(!s.isEmpty()) {
 				int elem = s.pop();
 				
+				while(next>elem) {
+					System.out.println(elem+"---"+next);
+					if(s.isEmpty()) {
+						break;
+					}
+					elem = s.pop();
+				}
+				
+				if(elem>next) {
+					s.push(elem);
+				}
 			}
-			
+			s.push(next);
 			
 		}
+		
+		System.out.println(s);
 	}
 
 	public static void main(String[] args) {
